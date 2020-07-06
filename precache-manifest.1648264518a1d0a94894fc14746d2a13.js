e46e82a87c45158083ea4faa53e8989c0f8b42b1
Dinesh Yadav <dinesh.yadav@loconav.com>
2020-07-06 21:53:33 +0530

    Added build file

---
+++ precache-manifest.1648264518a1d0a94894fc14746d2a13.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "02adfbdb6b4cf17499e1",
+    "url": "/static/css/main.deefc202.chunk.css"
+  },
+  {
+    "revision": "02adfbdb6b4cf17499e1",
+    "url": "/static/js/main.a2ceb5e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88ee9bfc2128a7eca892",
+    "url": "/static/css/2.daac4ddd.chunk.css"
+  },
+  {
+    "revision": "88ee9bfc2128a7eca892",
+    "url": "/static/js/2.17bdc623.chunk.js"
+  },
+  {
+    "revision": "3b5463fe95559a9b33c22030c04fb7f5",
+    "url": "/static/media/mainScaffold.3b5463fe.png"
+  },
+  {
+    "revision": "f3da08507ce1a22a2e6b2d5756b0f581",
+    "url": "/static/media/referralScaffold.f3da0850.png"
+  },
+  {
+    "revision": "e8fea423d55da15d55610598062ee92d",
+    "url": "/static/media/playstore.e8fea423.png"
+  },
+  {
+    "revision": "b901bfaed817be7cc363cb326315599c",
+    "url": "/static/media/appstore.b901bfae.png"
+  },
+  {
+    "revision": "44914fd2c34f1a81d789e41ac82b340f",
+    "url": "/static/media/reward.44914fd2.svg"
+  },
+  {
+    "revision": "1340f020215beee70ed99d9c6e32ae8e",
+    "url": "/static/media/accountManagement.1340f020.svg"
+  },
+  {
+    "revision": "a3fca37c70800fc8c8672c44a18cda42",
+    "url": "/static/media/smoothUpvoting.a3fca37c.svg"
+  },
+  {
+    "revision": "f738af00291d06644384fd56b481384c",
+    "url": "/static/media/textEditor.f738af00.svg"
+  },
+  {
+    "revision": "4a93eddbf84502b8e4732379c715ce7a",
+    "url": "/index.html"
+  }
+];
